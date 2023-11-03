b74d66c8c8fd7269d2285c81ebc8e563b272ce94
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2023-11-03 17:48:04 -0400

    random data points generation done and documented

--- tests/random_data_points_generator.py
+++ tests/random_data_points_generator.py
@@ -1,81 +1,166 @@
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import scipy
 
-from mpl_toolkits.mplot3d import Axes3D
+DEFAULT_POINTS_AMOUNT: int = 35
+RANDOM_DATA_INTERVAL: dict[str, float] = {"start": -10.0, "stop": 10.0}
+DEFAULT_Y: float = 0.0
 
-DEFAULT_POINTS_AMOUNT = 35
-RANDOM_DATA_INTERVAL = {"start": -10.0, "stop": 10.0}
-DEFAULT_Z = 0.0
+DEFAULT_X_Z_DISTANCE_RANGE: dict[str, float] = {"start": 2.5, "stop": 3.0}
+DEFAULT_Y_DISTANCE_RANGE: dict[str, float] = {"start": -0.1, "stop": 0.1}
 
-DEFAULT_X_Y_DISTANCE_RANGE = {"start": 2.5, "stop": 3.0}
-DEFAULT_Z_DISTANCE_RANGE = {"start": -0.1, "stop": 0.1}
+DEFAULT_THETA_RANGE: dict[str, float] = {"start": 0, "stop": 2 * math.pi}
 
 
 class RandomDataPointsGenerator:
-    points_amount = None
-    points = None
-    angle = None
-    _starting_x = None
-    _starting_y = None
-    _starting_z = None
+    points_amount: int = None
+    angle: float = None
+    _points: np.ndarray = None
+    _starting_x: np.array = None
+    _starting_y: np.array = None
+    _starting_z: np.array = None
+    _points_origin: np.array = None
+    _points_generated: bool = False
 
     def __init__(self, points_amount=DEFAULT_POINTS_AMOUNT) -> None:
         self.points_amount = points_amount
-        self.points = np.empty(points_amount, dtype=np.ndarray)
-        self.angle = 2 * np.pi / self.points_amount
-
-    def generate(self):
-        for index in range(self.points_amount):
-            print(self.point_from_origin(index))
-            self.points[index] = self.point_from_origin(index)
-
-    def point_from_origin(self, index):
-        distance = random.uniform(
-            DEFAULT_X_Y_DISTANCE_RANGE["start"], DEFAULT_X_Y_DISTANCE_RANGE["stop"]
-        )
-        z_distance = random.uniform(
-            DEFAULT_Z_DISTANCE_RANGE["start"], DEFAULT_Z_DISTANCE_RANGE["stop"]
-        )
-        x = self.starting_x + distance * np.cos(float(index) * self.angle)
-        y = self.starting_y + distance * np.sin(float(index) * self.angle)
-        z = self.starting_z + z_distance
-        return np.array([x, y, z], dtype=float)
+        # here we can assume that the points are ordered in the way they were captured
+        self._points = np.empty(points_amount, dtype=np.ndarray)
+        self.angle = 2 * float(math.pi) / float(self.points_amount)
 
     @property
-    def starting_x(self):
-        if self._starting_x is not None:
-            return self._starting_x
+    def points(self) -> np.ndarray[np.array]:
+        """
+        Randomly generated points from a points amounts.
+        Returns:
+            np.ndarray: Multidimensional array of 3D points that mimics the outline of a textile shape
+        """
+        if not self._points_generated:
+            self._generate()
+            self._random_transformation()
+            self._points_generated = True
+        return self._points
 
-        self._starting_x = random.uniform(
-            RANDOM_DATA_INTERVAL["start"], RANDOM_DATA_INTERVAL["stop"]
-        )
+    @property
+    def points_origin(self) -> np.array:
+        """
+        Returns the origin from which the points were created.
+        Returns:
+            np.array: The origin of the points.
+        """
+        if self._points_origin is None:
+            self._points_origin = np.array(
+                [self.starting_x, self.starting_y, self.starting_z]
+            )
+        return self._points_origin
+
+    @property
+    def starting_x(self) -> float:
+        """
+        Sets the x origin from where the points will be generated
+        Returns:
+            float: x position of the point origin
+        """
+        if self._starting_x is None:
+            self._starting_x = random.uniform(
+                RANDOM_DATA_INTERVAL["start"], RANDOM_DATA_INTERVAL["stop"]
+            )
+
         return self._starting_x
 
     @property
-    def starting_y(self):
-        if self._starting_y is not None:
-            return self._starting_y
+    def starting_y(self) -> float:
+        """
+        Sets the y origin from where the points will be generated
+        Returns:
+            float: y position of the point origin
+        """
+        if self._starting_y is None:
+            self._starting_y = DEFAULT_Y
 
-        self._starting_y = random.uniform(
-            RANDOM_DATA_INTERVAL["start"], RANDOM_DATA_INTERVAL["stop"]
-        )
         return self._starting_y
 
     @property
-    def starting_z(self):
-        if self._starting_z is not None:
-            return self._starting_z
+    def starting_z(self) -> float:
+        """
+        Sets the z origin from where the points will be generated
+        Returns:
+            float: z position of the point origin
+        """
+        if self._starting_z is None:
+            self._starting_z = random.uniform(
+                RANDOM_DATA_INTERVAL["start"], RANDOM_DATA_INTERVAL["stop"]
+            )
 
-        self._starting_z = DEFAULT_Z
         return self._starting_z
 
-    def plot(self):
+    def _generate(self) -> None:
+        """
+        Generates random 3D points to mimic the capturing of a 2D shape in a 3D environment.
+        The way they are dispersed will depend on the amounts of points that need to be generated.
+        """
+        for index in range(self.points_amount):
+            self._points[index] = self._point_from_origin(index)
+
+    def _point_from_origin(self, index: int) -> np.array:
+        """
+        Creates a point from the origin depending on the index, points amount and the angle.
+        Args:
+            index: Current index from which to create the point to.
+
+        Returns:
+            np.array: A point computed from the angle, index and points amount.
+        """
+        distance = random.uniform(
+            DEFAULT_X_Z_DISTANCE_RANGE["start"], DEFAULT_X_Z_DISTANCE_RANGE["stop"]
+        )
+        y_distance = random.uniform(
+            DEFAULT_Y_DISTANCE_RANGE["start"], DEFAULT_Y_DISTANCE_RANGE["stop"]
+        )
+        x = self.starting_x + distance * np.cos(float(index) * self.angle)
+        z = self.starting_z + distance * np.sin(float(index) * self.angle)
+        y = self.starting_y + y_distance
+        return np.array([x, y, z], dtype=float)
+
+    def _random_transformation(self) -> None:
+        """
+        Rotate all points around the current point origin in a random manner.
+        """
+        theta = random.uniform(
+            DEFAULT_THETA_RANGE["start"], DEFAULT_THETA_RANGE["stop"]
+        )
+        for index, point in enumerate(self._points):
+            self._points[index] = self._rotate_point(point, self.points_origin, theta)
+
+    @staticmethod
+    def _rotate_point(target_point: np.array, axis: np.array, theta: float) -> np.array:
+        """
+        Rotates a target point around an axis given a radian(theta).
+        Algorithms taken here: https://stackoverflow.com/questions/6802577/rotation-of-3d-vector
+        Args:
+            target_point: The point to be rotated.
+            axis: The axis from which the point will be rotated.
+            theta: The angle at which the point will be rotated.
+
+        Returns:
+            np.array: The rotated point.
+        """
+        rotation_matrix = scipy.linalg.expm(
+            np.cross(np.eye(3), axis / scipy.linalg.norm(axis) * theta)
+        )
+        rotate_point = np.dot(rotation_matrix, target_point)
+        return rotate_point
+
+    def plot(self) -> None:
+        """
+        Plots the points in a 3D environment
+        """
         x, y, z = zip(*self.points)
         figure = plt.figure()
         axe = figure.add_subplot(111, projection="3d")
         axe.scatter(x, y, z, c="b", marker="o")
-        axe.set_zlim(-1, 1)
         axe.set_xlabel("X Label")
         axe.set_ylabel("Y Label")
         axe.set_zlabel("Z Label")
@@ -85,5 +170,4 @@ class RandomDataPointsGenerator:
 
 if __name__ == "__main__":
     random_points_generator = RandomDataPointsGenerator()
-    random_points_generator.generate()
     random_points_generator.plot()
--- tests/test_random_points_generation.py
+++ tests/test_random_points_generation.py
@@ -1,10 +1,11 @@
 import unittest
+
+import numpy as np
+
 from random_data_points_generator import (
     RandomDataPointsGenerator,
     DEFAULT_POINTS_AMOUNT,
     RANDOM_DATA_INTERVAL,
-    DEFAULT_X_Y_DISTANCE_RANGE,
-    DEFAULT_Z_DISTANCE_RANGE,
 )
 
 
@@ -24,36 +25,15 @@ class TestRandomPointsGeneration(unittest.TestCase):
             <= RANDOM_DATA_INTERVAL["stop"]
         )
 
-    def test_starting_y(self):
+    def test_starting_z(self):
         self.assertTrue(
             RANDOM_DATA_INTERVAL["start"]
-            <= self.random_points_generator.starting_y
+            <= self.random_points_generator.starting_z
             <= RANDOM_DATA_INTERVAL["stop"]
         )
 
-    def test_starting_z(self):
-        self.assertAlmostEqual(self.random_points_generator.starting_z, 0.0)
-
-    def test_points_assignment(self):
-        self.random_points_generator.generate()
-
-    def test_z_points(self):
-        self.random_points_generator.generate()
-        for point in self.random_points_generator.points:
-            self.assertTrue(
-                DEFAULT_Z_DISTANCE_RANGE["start"]
-                <= point[2]
-                <= DEFAULT_Z_DISTANCE_RANGE["stop"]
-            )
-
-    def test_x_points(self):
-        self.random_points_generator.generate()
-        for point in self.random_points_generator.points:
-            self.assertTrue(
-                DEFAULT_X_Y_DISTANCE_RANGE["start"]
-                <= (self.random_points_generator.starting_x - point[0])
-                <= DEFAULT_X_Y_DISTANCE_RANGE["stop"]
-            )
+    def test_starting_y(self):
+        self.assertAlmostEqual(self.random_points_generator.starting_y, 0.0)
 
 
 if __name__ == "__main__":
